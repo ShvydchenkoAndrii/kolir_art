fa18092f934f8b53b6c6e8d7db5f485b6fc53604
unknown <shvydchenkoandriy@gmail.com>
2022-08-14 15:05:24 +0300

    add flip cards

--- js/master-class.js
+++ js/master-class.js
@@ -1,5 +1,29 @@
-const card = document.querySelector(".master-class_item");
+const card = document.querySelector(".master-class_item0");
 
 card.addEventListener("click", function (e) {
   card.classList.toggle('is-flipped');
+});
+
+const card1 = document.querySelector(".master-class_item1");
+
+card1.addEventListener("click", function (e) {
+  card1.classList.toggle('is-flipped');
+});
+
+const card2 = document.querySelector(".master-class_item2");
+
+card2.addEventListener("click", function (e) {
+  card2.classList.toggle('is-flipped');
+});
+
+const card3 = document.querySelector(".master-class_item3");
+
+card3.addEventListener("click", function (e) {
+  card3.classList.toggle('is-flipped');
+});
+
+const card4 = document.querySelector(".master-class_item4");
+
+card4.addEventListener("click", function (e) {
+  card4.classList.toggle('is-flipped');
 });
